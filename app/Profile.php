92a646794160ca2b9694a8cf0f9ed45089278497
alexandar89934 <aleksandar.radovanovic2712@gmail.com>
2022-07-31 20:08:31 +0200

    initial comit

---
+++ app/Profile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ?  $this->image : 'profile/rIEovEuW74pBlbnilGZ4XDjp6gAtn0eYdT7PsM40.png';
+
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome to freeCodeGram
+
+This is communitu of fellow developers and we love that you have joined us.
+
+All the best,<br>
+Aleksandar
+{{ config('app.name') }}
+@endcomponent
